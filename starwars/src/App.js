d36c0f885f196a5407da48706ea3c90e547ce0fc
martinezmarina <martinezmarina980@gmail.com>
2020-05-15 14:31:42 -0400

    got the minimum I beleive

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
+import Character from './components/Character'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+.contentContainer{
+  display:flex;
+flex-direction: row;
+flex-wrap:wrap;
+justify-content:space-evenly;
+}
+
+`
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,11 +22,26 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characterList, setCharacterList] = useState([])
+
+  useEffect(() => {
+    Axios.get(`https://swapi.py4e.com/api/people/`)
+    .then(res => {
+      setCharacterList(res.data.results)   
+    })
+    .catch(err => {
+      console.log('error')
+    })
+  }, [])
+  
 
   return (
-    <div className="App">
+    <StyledApp className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      <div className="contentContainer">
+      {characterList.map((characterObj) => <Character name={characterObj.name} height={characterObj.height} mass={characterObj.mass} hairColor={characterObj.hair_color} skinColor={characterObj.skin_color} homeWorld={characterObj.homeWorld} />)}
+      </div>
+    </StyledApp>
   );
 }
 
